fe0dcdb123fbabe14688e1e2384127751cf62b76
Yashwardhan Nigam <160997929+yashwardhan174@users.noreply.github.com>
2025-07-09 21:20:24 +0530

    Update activitylog.js

--- models/activitylog.js
+++ models/activitylog.js
@@ -1,25 +1,26 @@
 'use strict';
-const { Model } = require('sequelize');
-
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Activity extends Model {
+  class ActivityLog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      Activity.belongsTo(models.User, {
-        foreignKey: 'userId',
-        as: 'user'
-      });
+      // define association here
     }
   }
-
-  Activity.init({
+  ActivityLog.init({
     userId: DataTypes.INTEGER,
-    action: DataTypes.STRING,
-    entity: DataTypes.STRING,
-    entityId: DataTypes.INTEGER
+    projectId: DataTypes.INTEGER,
+    taskId: DataTypes.INTEGER,
+    action: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Activity',
+    modelName: 'ActivityLog',
   });
-
-  return Activity;
+  return ActivityLog;
 };
